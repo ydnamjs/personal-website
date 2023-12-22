4d33d07e529a4fdf9c757bd11d5229127aabdf47
michael <ydna.mjs@gmail.com>
2023-12-22 04:05:43 -0500

    fixed the leak caused where zoom and resize listeners would not be removed when the component using them was rerendered

--- src/customZoom.ts
+++ src/customZoom.ts
@@ -5,89 +5,6 @@ const MIN_ZOOM_LEVEL = 1;
 const MAX_ZOOM_LEVEL = 5;
 const ZOOM_INCREMENT = .25;
 
-function blockWheelZoom(event: WheelEvent) {
-
-    // metaKey is command on mac
-    if (event.ctrlKey || event.metaKey) {
-    event.preventDefault();
-    }
-};
-
-function blockCtrlZoom(event: KeyboardEvent) {
-
-    // metaKey is command on mac
-    if ((event.ctrlKey || event.metaKey) && (event.key === '+' || event.key === '-'|| event.key==='=')) {
-        event.preventDefault();
-    }
-};
-
-function setupZoomBlockListeners() {
-
-    window.addEventListener('wheel', blockWheelZoom, {passive: false});
-    window.addEventListener('keydown', blockCtrlZoom, {passive: false});
-    
-    return () => {
-        window.removeEventListener('wheel', blockWheelZoom);
-        window.removeEventListener('keydown', blockCtrlZoom);
-    };
-}
-
-function handleResize(setViewportHeight: Function, setViewportWidth: Function) {
-    setViewportWidth(window.innerWidth || document.documentElement.clientWidth);
-    setViewportHeight(window.innerHeight || document.documentElement.clientHeight);
-};
-
-function setupResizeListener(setViewportHeight: Function, setViewportWidth: Function) {
-    
-    const handleResizeCallback = () => {
-        handleResize(setViewportHeight, setViewportWidth)
-    }
-    
-    window.addEventListener('resize', handleResizeCallback);
-
-    return () => {
-      window.removeEventListener('resize', handleResizeCallback);
-    };
-}
-
-function setupZoomListeners(zoomLevel: number, setZoomLevel: Function) {
-    
-    const handleMouseZoom = (event: { ctrlKey: any; deltaY: number; }) => {
-        if (event.ctrlKey) {
-
-        const newZoomLevel = event.deltaY > 0 ? zoomLevel - ZOOM_INCREMENT : zoomLevel + ZOOM_INCREMENT;
-
-        const clampedZoomLevel = Math.min(Math.max(newZoomLevel, MIN_ZOOM_LEVEL), MAX_ZOOM_LEVEL);
-
-        setZoomLevel(clampedZoomLevel);
-        }
-    };
-
-    const handleButtonZoom = (event: KeyboardEvent) => {
-
-        if ((event.ctrlKey || event.metaKey) && (event.key === '+' || event.key==='=')) {
-            const newZoomLevel = zoomLevel + ZOOM_INCREMENT;
-            const clampedZoomLevel = Math.min(Math.max(newZoomLevel, MIN_ZOOM_LEVEL), MAX_ZOOM_LEVEL);
-            console.log(typeof setZoomLevel);
-            setZoomLevel(clampedZoomLevel);
-        }
-
-        if ((event.ctrlKey || event.metaKey) && (event.key === '-')) {
-            const newZoomLevel = zoomLevel - ZOOM_INCREMENT;
-            const clampedZoomLevel = Math.min(Math.max(newZoomLevel, MIN_ZOOM_LEVEL), MAX_ZOOM_LEVEL);
-            setZoomLevel(clampedZoomLevel);
-        }
-    }
-
-    window.addEventListener('wheel', handleMouseZoom);
-    window.addEventListener('keydown', handleButtonZoom);
-
-    return () => {
-        window.removeEventListener('wheel', handleMouseZoom);
-        window.removeEventListener('keydown', handleButtonZoom);
-    };
-}
-
 interface CustomZoomParams {
     setViewportHeight: Function,
     setViewportWidth: Function,
@@ -96,16 +13,84 @@ interface CustomZoomParams {
 }
 
 function useCustomZoom(zoomParams: CustomZoomParams) {
-    
-    useEffect(setupZoomBlockListeners, []);
-    
-    // the setters for the viewport height shouldn't be able to change if they can they need to be added to the dependencies array (currently empty)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => {setupResizeListener(zoomParams.setViewportHeight, zoomParams.setViewportWidth)}, [])
+/* 
+The reason this is so ugly is because it kind of has to be to prevent it from being even uglier.
 
-    // similarly to the useEffect for the resize listening, setZoomLevel shouldn't be able to change
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => {setupZoomListeners(zoomParams.zoomLevel, zoomParams.setZoomLevel)}, [zoomParams.zoomLevel]);
+TLDR: Moving the functions used in the listeners out of the useEffect makes you have to do a bunch of even uglier stuff for it to not leak.
+
+TSND: For reasons I do not know, if the function used by the listener (ex: handleResize) is defined outside the useEffect,
+it does not get removed when the component is rerendered. The reason for this has something to do with memoization.
+From what I've gathered, when the component gets rerendered, the instance of handleResize that is supposed to be removed from the listeners is lost
+if it isn't memoized, and defining it inside the useEffect makes it memoized. If we want to define it outside of the useEffect, 
+we'd have to memoize it ourselves, which public opinion seems to think is a pretty ugly thing to do.
+*/
+
+    useEffect(
+        () => {
+
+            const handleResize = () => {
+                zoomParams.setViewportWidth(window.innerWidth || document.documentElement.clientWidth);
+                zoomParams.setViewportHeight(window.innerHeight || document.documentElement.clientHeight);
+            }
+            
+            window.addEventListener('resize', handleResize);
+        
+            return () => {
+              window.removeEventListener('resize', handleResize);
+            };
+        }, 
+        [zoomParams]
+    )
+
+    useEffect(
+        () => {
+
+            const handleMouseZoom = (event: WheelEvent) => {
+
+                if (event.ctrlKey) {
+        
+                event.preventDefault();
+        
+                const newZoomLevel = event.deltaY > 0 ? zoomParams.zoomLevel - ZOOM_INCREMENT : zoomParams.zoomLevel + ZOOM_INCREMENT;
+                const clampedZoomLevel = Math.min(Math.max(newZoomLevel, MIN_ZOOM_LEVEL), MAX_ZOOM_LEVEL);
+        
+                zoomParams.setZoomLevel(clampedZoomLevel);
+                }
+            };
+        
+            const handleButtonZoom = (event: KeyboardEvent) => {
+        
+                if ((event.ctrlKey || event.metaKey) && (event.key === '+' || event.key==='=')) {
+        
+                    event.preventDefault();
+        
+                    const newZoomLevel = zoomParams.zoomLevel + ZOOM_INCREMENT;
+                    const clampedZoomLevel = Math.min(Math.max(newZoomLevel, MIN_ZOOM_LEVEL), MAX_ZOOM_LEVEL);
+        
+                    zoomParams.setZoomLevel(clampedZoomLevel);
+                }
+        
+                if ((event.ctrlKey || event.metaKey) && (event.key === '-')) {
+        
+                    event.preventDefault();
+        
+                    const newZoomLevel = zoomParams.zoomLevel - ZOOM_INCREMENT;
+                    const clampedZoomLevel = Math.min(Math.max(newZoomLevel, MIN_ZOOM_LEVEL), MAX_ZOOM_LEVEL);
+        
+                    zoomParams.setZoomLevel(clampedZoomLevel);
+                }
+            }
+        
+            window.addEventListener('wheel', handleMouseZoom, {passive: false});
+            window.addEventListener('keydown', handleButtonZoom, {passive: false});
+        
+            return () => {
+                window.removeEventListener('wheel', handleMouseZoom);
+                window.removeEventListener('keydown', handleButtonZoom);
+            };
+        }, 
+        [zoomParams]
+    );
 }
 
 export default useCustomZoom;
