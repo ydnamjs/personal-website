bd4f2bec1a67990de14d4e1371816ad9532ff8f5
ydnamjs <ydnamjs@udel.edu>
2024-01-28 19:35:20 -0500

    Added Navigation Bar template

---
+++ src/navigation-controller/sub-components/NavigationBar.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+
+export function NavigationBar({
+	categories,
+	selectedCategory,
+	setSelectedCategory,
+}: {
+	categories: string[];
+	selectedCategory: string;
+	setSelectedCategory: (category: string) => void;
+}): JSX.Element {
+	return <div />;
+}
