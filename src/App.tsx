34301a44371d6ca02af9e4c66bc556cab4b49b23
ydnamjs <ydnamjs@udel.edu>
2024-04-05 15:30:21 -0400

    Checkpoint Commit: background that matches the height of its sibling content div

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import pictureOfMe from "./assets/MichaelSnead.jpg";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
+		<div className="container">
+			<div className="main-panel">
+				<img src={pictureOfMe}></img>
+				<h1>About Me</h1>
+				<p>I am a person I like to code</p>
+				<p>This is a 2nd paragraph</p>
+				<p className="panel-text">
+					aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<span>test e</span>
-				<p>test e</p>
-			</header>
+				<h1>Education</h1>
+				<span className="panel-text">I went to school</span>
+				<span>
+					<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a
+					<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a
+					<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>a<br></br>E
+				</span>
+			</div>
+			<div className="background"></div>
 		</div>
 	);
 }
