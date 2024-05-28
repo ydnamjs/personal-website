69dd682038a0f458c4d9113730e70bb969aaef64
ydnamjs <ydnamjs@udel.edu>
2024-05-28 16:35:33 -0400

    Added document title with react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./App.css";
 import {MainPanel} from "./main-panel/MainPanel";
 import {AboutMe} from "./about-me/AboutMe";
+import {Helmet} from "react-helmet";
 
 function App() {
 	return (
 		<div className="container">
+			<Helmet>
+				<title>Michael Snead</title>
+			</Helmet>
 			<MainPanel>
 				<AboutMe></AboutMe>
 			</MainPanel>
