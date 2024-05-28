788014095678726ea99a5821eac3e7d6178d7a76
ydnamjs <ydnamjs@udel.edu>
2024-05-28 15:58:27 -0400

    Altered text to reflect that I graduated and am no longer taking machine learning

--- src/about-me/panels/Education.tsx
+++ src/about-me/panels/Education.tsx
@@ -6,14 +6,16 @@ export function Education() {
 		<div className="panel shadow">
 			<h2>2020-2024</h2>
 			<span>Bachelor of Science in Computer Science at University of Delaware</span>
-			<p>Cumulative GPA of 3.8 (as of 4/16/2024)</p>
+			<br />
+			<span>Concentration of AI and Robotics</span>
+			<p>Graduated With a Cumulative GPA of 3.8</p>
 			<h2>Favorite Classes:</h2>
 			<ul>
 				<li>CISC 275: Introduction to Software Engineering</li>
 				<li>CISC 372: Parallel Computing</li>
 				<li>CISC 374: Educational Game Development</li>
 				<li>CISC 437: Database Systems</li>
-				<li>CISC 484: Machine Learning (currently taking)</li>
+				<li>CISC 484: Machine Learning</li>
 			</ul>
 		</div>
 	);
