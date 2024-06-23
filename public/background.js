e4b4c41315efd19696964a0e3bc69bf37478764f
ydnamjs <ydnamjs@udel.edu>
2024-06-23 13:51:42 -0400

    Fixed background breaking in vertical direction and scale when zooming

--- public/background.js
+++ public/background.js
@@ -6,15 +6,15 @@ canvas.height = window.innerHeight;
 const SLIME_COLOR = "rgb(0, 255, 0)";
 const NUM_SLIMES = 500;
 
-const SLIME_START_HEIGHT = 0.1 * window.innerHeight;
-const SLIME_STOP_GROW_HEIGHT = 0.15 * window.innerHeight;
+const SLIME_START_HEIGHT = 0.1;
+const SLIME_STOP_GROW_HEIGHT = 0.15;
 
 const MAX_STARTING_RADIUS = 5;
 const MIN_GROWTH_RATE = 0.005;
 const MAX_GROWTH_RATE = 0.1;
 const MIN_FALL_SIZE = 10;
 const MAX_FALL_SIZE = 30;
-const GRAVITY = 0.01;
+const GRAVITY = 0.0001;
 
 class Slime {
 	constructor() {
@@ -38,14 +38,20 @@ class Slime {
 		if (this.y < SLIME_STOP_GROW_HEIGHT) {
 			this.radius += this.growthRate;
 		}
-		if (this.y > window.innerHeight) {
+		if (this.y * window.innerHeight > window.innerHeight) {
 			this.Reset();
 		}
 	}
 
 	Draw() {
 		context.beginPath();
-		context.arc(this.x * window.innerWidth, this.y, this.radius, 0, Math.PI * 2);
+		context.arc(
+			this.x * window.innerWidth,
+			this.y * window.innerHeight,
+			(this.radius * window.innerWidth) / 1600,
+			0,
+			Math.PI * 2,
+		);
 		context.fill();
 	}
 }
