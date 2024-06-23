44ff40ff2b146457f0787af7bce1b1b2be4f00ce
ydnamjs <ydnamjs@udel.edu>
2024-06-23 04:11:47 -0400

    Added a falling slime background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import {MainPanel} from "./main-panel/MainPanel";
 import {AboutMe} from "./about-me/AboutMe";
 import {Helmet} from "react-helmet";
 
+let scriptAttached = false;
+
 function App() {
+	useEffect(() => {
+		if (!scriptAttached) {
+			scriptAttached = true;
+			const script = document.createElement("script");
+			script.src = process.env.PUBLIC_URL + "/background.js";
+			script.async = true;
+			document.body.appendChild(script);
+			return () => {
+				script.remove();
+			};
+		}
+	}, []);
+
 	return (
 		<div className="container">
 			<Helmet>
@@ -13,7 +28,7 @@ function App() {
 			<MainPanel>
 				<AboutMe></AboutMe>
 			</MainPanel>
-			<div className="background"></div>
+			<canvas id="background" />
 		</div>
 	);
 }
