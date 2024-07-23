4e1b4de63e377b052bee3dad4a29de401bab3f2b
ydnamjs <ydnamjs@udel.edu>
2024-07-22 23:01:54 -0400

    Added navigation bar for switching between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import {Helmet} from "react-helmet";
 import {AboutMe} from "./pages/about-me/AboutMe";
+import {PageSelector} from "./PageSelector";
+
+export type PageType = "About" | "Portfolio" | "Other Interests" | "Contact + Links";
 
 function App() {
+	const [page, setPage] = useState<PageType>("About");
+
 	return (
 		<div className="background">
 			<Helmet>
 				<title>Michael Snead</title>
 			</Helmet>
-			<AboutMe></AboutMe>
+			<div className="navigation-bar">
+				<PageSelector page={page} setPage={setPage} buttonPage={"About"}></PageSelector>
+				<PageSelector page={page} setPage={setPage} buttonPage={"Portfolio"}></PageSelector>
+				<PageSelector
+					page={page}
+					setPage={setPage}
+					buttonPage={"Other Interests"}
+				></PageSelector>
+				<PageSelector
+					page={page}
+					setPage={setPage}
+					buttonPage={"Contact + Links"}
+				></PageSelector>
+			</div>
+			{page === "About" && <AboutMe></AboutMe>}
 		</div>
 	);
 }
