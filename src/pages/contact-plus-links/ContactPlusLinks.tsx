3cbc657059bf65dbfea8053c4cc22c95133a1913
ydnamjs <ydnamjs@udel.edu>
2024-07-23 20:38:40 -0400

    Reformatted contacts + links and made clickable links for everything also added a button for copying email address instead of mailto

--- src/pages/contact-plus-links/ContactPlusLinks.tsx
+++ src/pages/contact-plus-links/ContactPlusLinks.tsx
@@ -7,37 +7,76 @@ import linkedInWhite from "../../assets/contact-plus-links/LinkedIn_Icon_White.p
 import gitHubIconWhite from "../../assets/contact-plus-links/GitHub_Icon_White.png";
 
 export function ContactPlusLinks() {
+	{
+		/* 
+					<h2>
+						<a href="https://github.com/ydnamjs" target="_blank" rel="noreferrer">
+							GitHub Profile
+						</a>
+					</h2>
+					<button
+						className="copy-link-button"
+						onClick={() => {
+							navigator.clipboard.writeText("https://github.com/ydnamjs");
+						}}
+					>
+						Copy
+					</button> */
+	}
+
 	return (
 		<div className="contact-plus-links-page">
 			<div className="contact-plus-links--row">
-				<img src={emailIconWhite} className="contact-plus-links--icon"></img>
-				<div className="contact-plus-links--descripton">
-					<h2>Email</h2>
+				<div>
+					<a
+						className="contact-plus-links--section"
+						style={{height: "190px"}}
+						href="mailto:hireMichaelSnead@gmail.com"
+					>
+						<img src={emailIconWhite} className="contact-plus-links--icon"></img>
+						<h2 style={{margin: "5px"}}>Business Email</h2>
+						<br />
+						<span style={{textAlign: "center"}}>hireMichaelSnead@gmail.com</span>
+						<br />
+					</a>
+					<button
+						className="copy-email-button"
+						onClick={() => {
+							navigator.clipboard.writeText("hireMichaelSnead@gmail.com");
+						}}
+					>
+						Copy Email Address
+					</button>
 				</div>
-			</div>
-			<div className="contact-plus-links--row">
-				<div className="contact-plus-links--descripton">
+				<a
+					href="https://drive.google.com/file/d/1wlqSZAsHSnRj8TFKZZoO0wAmbaV-I8mI/view?usp=sharing"
+					target="_blank"
+					rel="noreferrer"
+					className="contact-plus-links--section"
+				>
+					<img src={resumeIconWhite} className="contact-plus-links--icon"></img>
 					<h2>Resume / CV</h2>
-				</div>
-				<img src={resumeIconWhite} className="contact-plus-links--icon"></img>
+				</a>
 			</div>
 			<div className="contact-plus-links--row">
-				<img src={linkedInWhite} className="contact-plus-links--icon"></img>
-				<div className="contact-plus-links--descripton">
+				<a
+					href="https://www.linkedin.com/in/michael-snead/"
+					target="_blank"
+					rel="noreferrer"
+					className="contact-plus-links--section"
+				>
+					<img src={linkedInWhite} className="contact-plus-links--icon"></img>
 					<h2>LinkedIn Profile</h2>
-				</div>
-			</div>
-			<div className="contact-plus-links--row">
-				<div className="contact-plus-links--descripton">
+				</a>
+				<a
+					href="https://www.github.com/ydnamjs"
+					target="_blank"
+					rel="noreferrer"
+					className="contact-plus-links--section"
+				>
+					<img src={gitHubIconWhite} className="contact-plus-links--icon"></img>
 					<h2>GitHub Profile</h2>
-					<div>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla qui nobis
-						facere maxime nemo, cum possimus veritatis ratione dolor id adipisci!
-						Perspiciatis consequatur nobis blanditiis doloribus placeat qui esse
-						recusandae!
-					</div>
-				</div>
-				<img src={gitHubIconWhite} className="contact-plus-links--icon"></img>
+				</a>
 			</div>
 		</div>
 	);
