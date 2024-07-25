7d68286a6449a1ba974afe1f00707265538b6343
ydnamjs <ydnamjs@udel.edu>
2024-07-25 02:02:23 -0400

    Added basic side layout items for portfolio item component

--- src/pages/portfolio/pieces/PortfolioItem.tsx
+++ src/pages/portfolio/pieces/PortfolioItem.tsx
@@ -4,20 +4,27 @@ import "../Portfolio.css";
 
 export function PortfolioItem() {
 	return (
-		<div className="portfolio-section portfolio-item">
-			<h2 className="portfolio-item--heading">Corkboard Website</h2>
-			<p>
-				Basic &quot;drag and drop&quot; web application that can be used to create note
-				cards on a corkboard
-			</p>
-			<div className="tech-used">
-				<p>React</p>
-				<p>TypeScript</p>
-				<p>HTML</p>
-				<p>CSS</p>
-				<p>Git</p>
-				<p>GitHub</p>
-				<p>React DnD</p>
+		<div className="portfolio-item">
+			<div className="portfolio-section portfolio-item--info">
+				<h2 className="portfolio-item--heading">Corkboard Website</h2>
+				<p>
+					Basic &quot;drag and drop&quot; web application that can be used to create note
+					cards on a corkboard
+				</p>
+				<div className="tech-used">
+					<p>React</p>
+					<p>TypeScript</p>
+					<p>HTML</p>
+					<p>CSS</p>
+					<p>Git</p>
+					<p>GitHub</p>
+					<p>React DnD</p>
+				</div>
+			</div>
+			<div className="portfolio-item--links">
+				<p>Live Deployment</p>
+				<p>Source Code</p>
+				<p>Images</p>
 			</div>
 		</div>
 	);
