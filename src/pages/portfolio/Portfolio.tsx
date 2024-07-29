c787e3c2a58b04a9691a2eaec764878e02ed1d79
ydnamjs <ydnamjs@udel.edu>
2024-07-29 01:11:06 -0400

    Parameterized portfolio item component

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,10 +1,61 @@
 import React, {useState} from "react";
 import "./Portfolio.css";
+import "./pieces/PortfolioItem.css";
 
 export type Portfolio = "Web Dev" | "Machine Learning" | "Games";
 
 import {PortfolioButton} from "./pieces/PortfolioButton";
 import {PortfolioItem} from "./pieces/PortfolioItem";
+
+const linkButtons = [
+	<a
+		className="portfolio-item--links-item"
+		key={"testA"}
+		href="https://bkbranson.github.io/team-2-final-project/"
+		target="_blank"
+		rel="noreferrer"
+	>
+		Live Deployment
+	</a>,
+	<a
+		className="portfolio-item--links-item"
+		key={"testB"}
+		href="https://github.com/bkbranson/team-2-final-project"
+		target="_blank"
+		rel="noreferrer"
+	>
+		Source Code
+	</a>,
+];
+const linkButtons2 = [
+	<a
+		className="portfolio-item--links-item"
+		key={"testE"}
+		href="https://devpost.com/software/sound-ificator-inator"
+		target="_blank"
+		rel="noreferrer"
+	>
+		Dev Post
+	</a>,
+	<a
+		className="portfolio-item--links-item"
+		key={"testE"}
+		href="https://www.youtube.com/watch?v=qyBQygNTVF8"
+		target="_blank"
+		rel="noreferrer"
+	>
+		Video Demo
+	</a>,
+	<a
+		className="portfolio-item--links-item"
+		key={"testE"}
+		href="https://github.com/henhackz/henhacks.github.io"
+		target="_blank"
+		rel="noreferrer"
+	>
+		Source Code
+	</a>,
+];
 
 export function Portfolio() {
 	const [activePortfolio, setActivePortfolio] = useState<Portfolio>("Web Dev");
@@ -28,7 +79,48 @@ export function Portfolio() {
 					buttonPortfolio={"Games"}
 				></PortfolioButton>
 			</div>
-			<PortfolioItem></PortfolioItem>
+			<PortfolioItem
+				title={"Sound-ificator-inator"}
+				description={
+					<>
+						<p>
+							A react web app that simulates a 3D sound enviornment that reacts to
+							rotations in real time using gyro orientations recieved from a raspberry
+							pi. Intended to reduce overstimulation in noisy environments for
+							neurodivergent people.
+						</p>
+						<p>
+							Created for and awarded 1st place in the 2024 Hen Hacks Hackathon Best
+							Accessibilty Hack Category.
+						</p>
+					</>
+				}
+				techUsed={[
+					"JavaScript",
+					"three.js",
+					"HTML",
+					"CSS",
+					"Git",
+					"GitHub",
+					"Raspberry Pi",
+					"API",
+				]}
+				linkButtons={linkButtons2}
+			></PortfolioItem>
+			<PortfolioItem
+				title={"Corkboard Website"}
+				description={
+					<p>
+						Basic &quot;drag and drop&quot; web application that can be used to create
+						note cards on a corkboard Lorem ipsum dolor sit, amet consectetur
+						adipisicing elit. Veniam facilis esse delectus beatae laborum harum
+						quibusdam illo. Harum illum adrem fuga omnis assumenda, velit optio voluptas
+						repudiandae aspernatur doloribus.
+					</p>
+				}
+				techUsed={["React", "Typescript", "HTML", "CSS", "Git", "GitHub", "React DnD"]}
+				linkButtons={linkButtons}
+			></PortfolioItem>
 		</div>
 	);
 }
--- src/pages/portfolio/pieces/PortfolioItem.tsx
+++ src/pages/portfolio/pieces/PortfolioItem.tsx
@@ -1,39 +1,30 @@
-import React from "react";
+import React, {ReactElement} from "react";
 import "./PortfolioItem.css";
 import "../Portfolio.css";
 
-export function PortfolioItem() {
+export function PortfolioItem({
+	title,
+	description,
+	techUsed,
+	linkButtons,
+}: {
+	title: string;
+	description: ReactElement;
+	techUsed: string[];
+	linkButtons: ReactElement[];
+}) {
 	return (
 		<div className="portfolio-item">
 			<div className="portfolio-section portfolio-item--info">
-				<h2 className="portfolio-item--heading">Corkboard Website</h2>
-				<p>
-					Basic &quot;drag and drop&quot; web application that can be used to create note
-					cards on a corkboard Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					Veniam facilis esse delectus beatae laborum harum quibusdam illo. Harum illum ad
-					rem fuga omnis assumenda, velit optio voluptas repudiandae aspernatur doloribus.
-				</p>
+				<h2 className="portfolio-item--heading">{title}</h2>
+				{description}
 				<div className="tech-used">
-					<p>React</p>
-					<p>TypeScript</p>
-					<p>HTML</p>
-					<p>CSS</p>
-					<p>Git</p>
-					<p>GitHub</p>
-					<p>React DnD</p>
+					{techUsed.map((item, index) => {
+						return <p key={"tech-used-" + title + index}>{item}</p>;
+					})}
 				</div>
 			</div>
-			<div className="portfolio-item--links">
-				<div className="portfolio-item--links-item">
-					<p>Live Deployment</p>
-				</div>
-				<div className="portfolio-item--links-item">
-					<p>Live Deployment</p>
-				</div>
-				<div className="portfolio-item--links-item">
-					<p>Live Deployment</p>
-				</div>
-			</div>
+			<div className="portfolio-item--links">{linkButtons}</div>
 		</div>
 	);
 }
