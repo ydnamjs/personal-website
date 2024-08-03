fdf747501a1a22580573b0aef8343fd5c1b23041
ydnamjs <ydnamjs@udel.edu>
2024-08-03 02:23:33 -0400

    Removed dependency on javascript for handling carousel width

--- src/pages/other-interests/GameCard.tsx
+++ src/pages/other-interests/GameCard.tsx
@@ -5,7 +5,9 @@ export function GameCard({src, title}: {src: string; title: string}) {
 	return (
 		<div className="carousel-card">
 			<img src={src} className="carousel-card--image"></img>
-			<p className="panel carousel-card--label">{title}</p>
+			<div className="panel carousel-card--label carousel-card--label-single-line">
+				<p>{title}</p>
+			</div>
 		</div>
 	);
 }
